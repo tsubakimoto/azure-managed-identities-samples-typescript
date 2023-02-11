90fd6bbabfbe1ed552f1d9afa56ca8f698aa3ee3
Yuta Matsumura <tkmbmbr@gmail.com>
2023-02-11 14:47:24 +0000

    Add index.pug

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
+const path = require('path');
+
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 import express from "express";
 const app: express.Express = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 const port = process.env.PORT || 3000
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!')
+  res.render('index', {
+    title: 'Hey',
+    message: 'Hello there!'
+  })
 })
 
 app.listen(port, () => {
