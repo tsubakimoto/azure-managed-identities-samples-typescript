c88958282ae85aeff46025c9fa53d292d954e292
Yuta Matsumura <tkmbmbr@gmail.com>
2023-02-11 13:38:03 +0000

    update express app

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## base
 
-https://learn.microsoft.com/en-us/azure/app-service/quickstart-nodejs
+- https://learn.microsoft.com/en-us/azure/app-service/quickstart-nodejs
+- https://qiita.com/hinaqiita/items/fe0ea4b9f71cb0f7c7f1
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app: express.Express = express()
+const port = 3000
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
