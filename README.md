7d075a3c956f2650cc6e0f63dfd2109d458efe8c
Yuta Matsumura <tkmbmbr@gmail.com>
2023-02-12 13:24:27 +0000

    Add storage list page

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## base
 
 - https://learn.microsoft.com/en-us/azure/app-service/quickstart-nodejs
-- https://qiita.com/hinaqiita/items/fe0ea4b9f71cb0f7c7f1
 - https://learn.microsoft.com/en-us/azure/storage/common/storage-samples-javascript
+- https://qiita.com/hinaqiita/items/fe0ea4b9f71cb0f7c7f1
+- https://blog.logrocket.com/how-to-set-up-node-typescript-express/
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 import { DefaultAzureCredential } from "@azure/identity";
 
-const path = require('path');
-
 require("dotenv").config();
 
 import express from "express";
 const app: express.Express = express()
+// const path = require('path');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 const port = process.env.PORT || 3000
 
 app.get('/', async (req: express.Request, res: express.Response) => {
+  res.send('<h1>Azure Managed Identities</h1><h2>Storage</h2><a href="/storage/">List</a>')
+})
+
+app.get('/storage/', async (req: express.Request, res: express.Response) => {
   const account = process.env.ACCOUNT_NAME
   const blobServiceClient = new BlobServiceClient(
     `https://${account}.blob.core.windows.net`,
@@ -24,19 +27,14 @@ app.get('/', async (req: express.Request, res: express.Response) => {
   // Create a container
   const containerName = 'images';
   const containerClient = await blobServiceClient.getContainerClient(containerName);
-  const createContainerResponse = containerClient.createIfNotExists();
+  containerClient.createIfNotExists();
 
-  // console.log(`Created container ${containerName} successfully`, createContainerResponse.requestId);
-
-  console.log("Blobs:");
+  let blobs: string = '';
   for await (const blob of containerClient.listBlobsFlat()) {
-    console.log(`- ${blob.name}`);
+    blobs = blobs + `<li>${blob.name}</li>`;
   }
     
-  res.render('index', {
-    title: 'Hey',
-    message: 'Hello there!'
-  })
+  res.send(`<h1>Storage list</h1><ul>${blobs}</ul>`)
 })
 
 app.listen(port, () => {
